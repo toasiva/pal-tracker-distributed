81329ae4c1b4ffedddfb63a97a28199c71268164
toasiva <siva.arumugham@cognizant.com>
2020-05-19 13:27:05 +0530

    Debug

--- databases/create_databases.sql
+++ databases/create_databases.sql
@@ -1,11 +1,11 @@
-DROP DATABASE tracker_allocations_dev;
-DROP DATABASE tracker_backlog_dev;
-DROP DATABASE tracker_registration_dev;
-DROP DATABASE tracker_timesheets_dev;
-DROP DATABASE tracker_allocations_test;
-DROP DATABASE tracker_backlog_test;
-DROP DATABASE tracker_registration_test;
-DROP DATABASE tracker_timesheets_test;
+DROP DATABASE IF EXISTS tracker_allocations_dev;
+DROP DATABASE IF EXISTS tracker_backlog_dev;
+DROP DATABASE IF EXISTS tracker_registration_dev;
+DROP DATABASE IF EXISTS tracker_timesheets_dev;
+DROP DATABASE IF EXISTS tracker_allocations_test;
+DROP DATABASE IF EXISTS tracker_backlog_test;
+DROP DATABASE IF EXISTS tracker_registration_test;
+DROP DATABASE IF EXISTS tracker_timesheets_test;
 
 CREATE USER IF NOT EXISTS 'tracker'@'localhost'
   identified by '';
